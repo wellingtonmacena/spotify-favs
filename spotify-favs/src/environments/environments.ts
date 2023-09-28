0aab8fd02e06d215934c07dcdbb922581c950b83
wellingtonmacena <wellington.macena.23@gmail.com>
2023-09-28 19:29:11 -0300

    Removed env file

--- spotify-favs/src/environments/environments.ts
+++
@@ -1,7 +0,0 @@
-export const environment ={
-  production: false,
-  spotifyClientId: '84e233434f434c5086c638405f7420fe',
-  spotifyClientSecret: 'b7cd3573fe7e48ce9d66119c43faf228',
-  spotifyReadScope: 'user-read-private user-read-email',
-  spotifyRedirectURI: 'http://localhost:4200/callback',
-}
