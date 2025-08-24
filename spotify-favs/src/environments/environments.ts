34b8e2e188ca4de190a7ea0b9605a7728eea336c
Wellington <wellington.macena.23@gmail.com>
2025-08-24 00:27:36 -0300

    Add environment configuration file for Spotify API integration

---
+++ spotify-favs/src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environment ={
+  production: false,
+  spotifyClientId: '', //insert your Spotify Client ID
+  spotifyClientSecret: '' ,//insert your Spotify Client Secret
+  spotifyReadScope: 'user-read-private user-read-email user-top-read playlist-read-private playlist-modify-private playlist-modify-public playlist-read-collaborative',
+  spotifyRedirectURI: 'http://localhost:4200/callback',
+}
